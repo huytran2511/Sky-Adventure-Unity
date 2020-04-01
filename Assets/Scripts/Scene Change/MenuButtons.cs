584047e65dad9773aaf3ee878e30c879cdb39ff9
Huy Tran <tranquanghuy999@gmail.com>
2020-04-01 15:07:48 -0700

    finish main menu scene and create volume slider to change volume

---
+++ Assets/Scripts/Scene Change/MenuButtons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void PlayGame()
+    {
+
+    }
+    public void GoHome()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
