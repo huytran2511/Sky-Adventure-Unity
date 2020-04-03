433a39d48683bece96e4278f709b4b65b900bb16
Huy Tran <tranquanghuy999@gmail.com>
2020-04-03 16:56:19 -0700

    let player land on platform

--- Assets/Scripts/Gameplay/Player.cs
+++ Assets/Scripts/Gameplay/Player.cs
@@ -7,7 +7,9 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D playerBody;
     private Button jumpButton;
-    
+    private bool hasJumped;
+    private GameObject parent;
+        
     // Start is called before the first frame update
     void Awake()
     {
@@ -20,17 +22,50 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(hasJumped && playerBody.velocity.y == 0)
+        {
+            hasJumped = false;
+            transform.SetParent(parent.transform);
+        }
+        else if(parent != null)
+        {
+            transform.SetParent(parent.transform);
+        }
     }
     public void Jump()
     {
         if(playerBody.velocity.y == 0)
         {
+            playerBody.velocity = new Vector2(0, 10);
+            hasJumped = true;
             //if (Input.GetKeyDown("space"))
             //{
             //    playerBody.velocity = new Vector2(0, 10);
             //}
-            playerBody.velocity = new Vector2(0, 10);
         }
     }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Platform")
+        {
+            parent = collision.gameObject;
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            parent = null;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+
+    }
+
+
 }
