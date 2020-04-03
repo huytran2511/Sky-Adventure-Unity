84f62f6037abba12c197904b662cb3271debd5b4
Huy Tran <tranquanghuy999@gmail.com>
2020-04-03 01:40:00 -0700

    adding jump feature and player can land on the platform

---
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D playerBody;
+    private Button jumpButton;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        jumpButton = GameObject.Find("JumpButton").GetComponent<Button>();
+        jumpButton.onClick.AddListener(() => Jump());
+
+        playerBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Jump()
+    {
+        if(playerBody.velocity.y == 0)
+        {
+            playerBody.velocity = new Vector2(0, 10);
+        }
+    }
+}
